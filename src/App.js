adfcbf952b2769fdd5686b333b3ea6d3cc764ff5
Izzah <l201314@lhr.nu.edu.pk>
2023-01-15 17:24:44 +0500

    setup gh-pages

---
+++ src/App.js
@@ -0,0 +1,94 @@
+
+import { useEffect, useState } from 'react';
+import { nanoid } from 'nanoid';
+import NotesList from "./components/NotesList";
+import Search from './components/Search';
+import Header from './components/Header';
+
+const App = () => {
+
+  const [notes, setNotes] = useState([
+
+    {
+      id: nanoid(),
+      text:'First note',
+      date:'14/04/2021'
+    },
+    {
+      id: nanoid(),
+      text:'Use toggle mode to enable / disable dark mode',
+      date:'15/04/2021'
+    },
+    {
+      id: nanoid(),
+      text:'You can add, delete or search any amount of notes you like',
+      date:'16/04/2021'
+    }
+  ]);
+
+  const [searchText, setSearchText] = useState('');
+  const [darkMode, setDarkMode] = useState(false);
+
+  useEffect(() => {
+    
+    const savedNotes = JSON.parse(
+
+      localStorage.getItem(
+        'react-customized-notepad-data'
+      )
+    );
+
+    if(savedNotes){
+      setNotes(savedNotes);
+    }
+
+  }, []);
+
+  useEffect(() => {
+
+    localStorage.setItem(
+        'react-customized-notepad-data',
+        JSON.stringify(notes)
+      );
+
+  }, [notes]);
+
+  const addNote = (text) => {
+
+    const date = new Date();
+
+    const newNote = {
+      id:nanoid(),
+      text: text,
+      date: date.toLocaleDateString()
+    };
+
+    const newNotes = [...notes, newNote];
+    setNotes(newNotes);
+  }
+
+  const deleteNote = (id) => {
+
+    const newNotes = notes.filter((note) => note.id !== id );
+    setNotes(newNotes);
+
+  }
+
+  return(
+    <div className = {`${darkMode && 'dark-mode'}`}>
+      <div className="container">
+        <Header handleToggleDarkMode = {setDarkMode}/>
+        <Search handleSearchNote = {setSearchText}/>
+        <NotesList
+          notes = {notes.filter((note) => 
+            note.text.toLowerCase().includes(searchText))
+          }
+          handleAddNote = {addNote} 
+          handleDeleteNote = {deleteNote}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default App;
